d4d64f7efc40bab25b18108088e6aef03cdd2693
Sebastian villegas <sebasvil20@gmail.com>
2020-09-28 15:37:28 -0500

    Updates

---
+++ precache-manifest.edd018c9b7a28ca2c91c6b63c1685722.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f02679edbd433c4d875daee05c7458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c7af77bce7abc032c2a",
+    "url": "/static/css/main.7a910cb6.chunk.css"
+  },
+  {
+    "revision": "c3fb769e5f3ed09cd07c",
+    "url": "/static/js/2.4d728d23.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.4d728d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7af77bce7abc032c2a",
+    "url": "/static/js/main.469ec35e.chunk.js"
+  },
+  {
+    "revision": "6fc353a81f8d16a7f871",
+    "url": "/static/js/runtime-main.7035a823.js"
+  },
+  {
+    "revision": "fcab096edba1c7a2a1acbc3cb44922cb",
+    "url": "/static/media/Foto-personal-Circle.fcab096e.png"
+  },
+  {
+    "revision": "3874f1b78409370925a8087792b3432c",
+    "url": "/static/media/NodeJs.3874f1b7.png"
+  },
+  {
+    "revision": "96e4aeda006e9d5615a3bd8b74fd50af",
+    "url": "/static/media/mysql.96e4aeda.png"
+  },
+  {
+    "revision": "af73fd1208da401fbe8f3fdf9a505a8d",
+    "url": "/static/media/reactw.af73fd12.png"
+  }
+]);
