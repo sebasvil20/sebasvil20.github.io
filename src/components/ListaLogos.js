f989361ce6eb217dd216d2aebf5304efb2aee4b8
Sebastian villegas <sebasvil20@gmail.com>
2020-09-29 08:32:08 -0500

    responsive update
    Se hacen cambios para arreglar vista en los celulares de la nueva seccion de certificaciones

--- src/components/ListaLogos.js
+++ src/components/ListaLogos.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ListaLogos = ({ imagen }) => {
   return (
-    <div className="imagen-tecnologias ml-2 flex justify-center align-center z-50 ">
+    <div className="imagen-tecnologias ml-2 mb-2 flex justify-center align-center z-50 ">
       <img src={imagen} alt="Logo" className=" z-50 imagen-tecnologias" />
     </div>
   );
