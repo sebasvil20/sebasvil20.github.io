f567a00a9ac8880822a88e142d05764e3541f050
Sebastian Villegas <sebasvil20@gmail.com>
2021-04-27 14:21:46 -0500

    Trying cool background effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { About } from './components/About'
 import { Footer } from './components/Footer'
 import { Hero } from './components/Hero'
@@ -6,8 +6,17 @@ import { Menu } from './components/Menu'
 import { Works } from './components/Works'
 
 function App() {
+  const [color, saveColor] = useState("#ff5851")
+
+  window.addEventListener('scroll', () => {
+    let y = 1 + (window.scrollY || window.pageYOffset) / 150
+    y = y < 1 ? 1 : y // ensure y is always >= 1 (due to Safari's elastic scroll)
+    console.log(y)
+    saveColor( y > 13 ? "#6B0990": y> 9 ? "#EF3158" : y > 6 ? "#E86618" : "#ff5851")
+  })
+
   return (
-    <section className='main-container'>
+    <section className='main-container' style={{backgroundColor: color}}>
       <Menu />
       <Hero />
       <About />
